094a988caa7ba0ec02336b432ea0582984cdc1be
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2022-04-23 19:26:07 +0100

    preview page done

---
+++ view/index_files/view.js
@@ -0,0 +1,41 @@
+// const baseURL = "https://nftdynamo.herokuapp.com"
+const baseURL = "http://localhost:3010"
+/**
+ * This function handles getting ipfs data from the backend service.
+ */
+const getIPFSData = async (code) => {
+    try {
+        const payload = await fetch(`${baseURL}/preview`, {
+            method: 'GET', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+                sesID: code
+            },
+        });
+        const ress = await payload.json();
+        const imagePath = ress.data.imageFolderLink;
+        document.getElementById("ipfsDisplay").src = imagePath;
+        document.getElementById("nftTitle").innerText = ress.data.title;
+        document.title = ress.data.title + " - NFT Dynamo";
+        document.getElementById("nftDescription").innerText = ress.data.description;
+        // Swal.fire({
+        //     title: 'Sweet!',
+        //     text: '',
+        //     imageUrl: ress.imageInfo[0].path,
+        //     imageWidth: 300,
+        //     imageHeight: 300,
+        //     imageAlt: 'Custom image',
+        // });
+    } catch (err) {
+        console.log(err, "<==========");
+    }
+}
+
+
+const params = new URLSearchParams(window.location.search);
+const code = params.get('code');
+console.log(code)
+if (code) {
+    getIPFSData(code);
+}
+
